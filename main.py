d05c14c07a7b4aacdeb199a07ec5684c89fb4435
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2023-05-23 15:54:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,55 @@
-import streamlit as st
 import pandas as pd
 
-def load_data(upload):
-    data = pd.read_csv(upload)
-    data = data.drop(data.index[-2:])
-    data['FinalKey'] = data['FinalKey'].str.upper()
-    return data
+#### Upload PPV Offers ####
+ppv_offers = pd.read_csv(r'C:\Users\3590080\OneDrive - Jabil\Desktop\Weekly Offers.csv')
+# Determine the last two rows
+last_two_rows = ppv_offers.iloc[-2:, :]
+# Drop the last two rows
+ppv_offers = ppv_offers.drop(ppv_offers.index[-2:])
+# Make columns upper case
+ppv_offers['Offer MPN'] = ppv_offers['Offer MPN'].str.upper()
+ppv_offers['STD MPN'] = ppv_offers['STD MPN'].str.upper()
+ppv_offers['Offer JPN'] = ppv_offers['Offer JPN'].str.upper()
+ppv_offers['STD JPN'] = ppv_offers['STD JPN'].str.upper()
+ppv_offers['JPN'] = ppv_offers['JPN'].str.upper()
+ppv_offers['Site'] = ppv_offers['Site'].str.upper()
+ppv_offers['Offer Site'] = ppv_offers['Offer Site'].str.upper()
+ppv_offers['STD Site'] = ppv_offers['STD Site'].str.upper()
+ppv_offers['FinalKey'] = ppv_offers['FinalKey'].str.upper()
+# Delete PPV Offer rows where JPN != JPN
+ppv_offers = ppv_offers[ppv_offers['Offer JPN'] == ppv_offers['STD JPN']]
+# Delete PPV Offer rows where MPN != MPN
+ppv_offers = ppv_offers[ppv_offers['Offer MPN'] == ppv_offers['STD MPN']]
+# Delete PPV Offer rows where Site != Site
+ppv_offers = ppv_offers[ppv_offers['Offer Site'] == ppv_offers['STD Site']]
 
-def process_ppv_offers(data):
-    data['Offer MPN'] = data['Offer MPN'].str.upper()
-    data['STD MPN'] = data['STD MPN'].str.upper()
-    data['Offer JPN'] = data['Offer JPN'].str.upper()
-    data['STD JPN'] = data['STD JPN'].str.upper()
-    data['JPN'] = data['JPN'].str.upper()
-    data['Site'] = data['Site'].str.upper()
-    data['Offer Site'] = data['Offer Site'].str.upper()
-    data['STD Site'] = data['STD Site'].str.upper()
-    data['FinalKey'] = data['FinalKey'].str.upper()
 
-    data = data[data['Offer JPN'] == data['STD JPN']]
-    data = data[data['Offer MPN'] == data['STD MPN']]
-    data = data[data['Offer Site'] == data['STD Site']]
+#### Upload SAT Quotes ####
+sat_quotes = pd.read_csv(r'C:\Users\3590080\OneDrive - Jabil\Desktop\SAT Quotes.csv')
+# Determine the last two rows
+last_two_rows = sat_quotes.iloc[-2:, :]
+# Drop the last two rows
+sat_quotes = sat_quotes.drop(sat_quotes.index[-2:])
+# Make columns upper case
+sat_quotes['FinalKey'] = sat_quotes['FinalKey'].str.upper()
 
-    return data
 
-def main():
-    st.title("CSV Data Processor")
-    ppv_file = st.file_uploader("Upload PPV Offers", type=['csv'])
-    sat_file = st.file_uploader("Upload SAT Quotes", type=['csv'])
-    open_file = st.file_uploader("Upload Open Orders", type=['csv'])
+#### Upload Open Orders ####
+open_orders = pd.read_csv(r'C:\Users\3590080\OneDrive - Jabil\Desktop\Open Orders.csv')
+# Make sure that 'POCreateDate' is a datetime object
+open_orders['POCreateDate'] = pd.to_datetime(open_orders['POCreateDate'])
+# Sort the DataFrame by 'POCreateDate' in descending order (most recent first)
+open_orders = open_orders.sort_values('POCreateDate', ascending=False)
+# Drop duplicates based on 'FinalKey', keeping the first occurrence (which is the most recent due to the sorting)
+open_orders = open_orders.drop_duplicates(subset='FinalKey', keep='first')
+# Drop the last two rows
+last_two_rows = open_orders.iloc[-2:, :]
+open_orders = open_orders.drop(open_orders.index[-2:])
+# Make columns upper case
+open_orders['FinalKey'] = open_orders['FinalKey'].str.upper()
 
-    if ppv_file is not None and sat_file is not None and open_file is not None:
-        ppv_offers = load_data(ppv_file)
-        ppv_offers = process_ppv_offers(ppv_offers)
-        sat_quotes = load_data(sat_file)
-        open_orders = load_data(open_file)
+# Merge ppv_offers with sat_quotes on 'FinalKey' using a left join
+merged_data = ppv_offers.merge(sat_quotes, on='FinalKey', how='left')
 
-        merged_data = ppv_offers.merge(sat_quotes, on='FinalKey', how='left')
-        # merged_data = merged_data.merge(open_orders, on='FinalKey', how='left')
-
-        st.write(merged_data)
-
-        csv = merged_data.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()  
-        href = f'<a href="data:file/csv;base64,{b64}" download="merged_data.csv">Download CSV File</a>'
-        st.markdown(href, unsafe_allow_html=True)
-
-if __name__ == "__main__":
-    main()
-
+# Merge the merged_data with open_orders on 'FinalKey' using a left join
+merged_data = merged_data.merge(open_orders, on='FinalKey', how='left
