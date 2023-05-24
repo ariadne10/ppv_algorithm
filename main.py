18f1456532813b289d818fd38ea848e2ac50ca2b
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2023-05-24 15:18:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_table_download_link(df):
     return href
 
 # Display a file uploader widget in your app for each Excel file
-ppv_offers_file = st.file_uploader("Upload Weekly Offers Excel", type=['xlsx'])
+ppv_offers_file = st.file_uploader("Upload PPV Offers Excel", type=['xlsx'])
 sat_quotes_file = st.file_uploader("Upload SAT Quotes Excel", type=['xlsx'])
 open_orders_file = st.file_uploader("Upload Open Orders Excel", type=['xlsx'])
 
@@ -22,7 +22,6 @@ if ppv_offers_file and sat_quotes_file and open_orders_file:
     ppv_offers = pd.read_excel(ppv_offers_file)
     sat_quotes = pd.read_excel(sat_quotes_file)
     open_orders = pd.read_excel(open_orders_file)
-    
     
     # Preprocessing and merging code here
 
@@ -39,7 +38,9 @@ if ppv_offers_file and sat_quotes_file and open_orders_file:
     sat_quotes = sat_quotes.iloc[:-2, :]
     sat_quotes['FinalKey'] = sat_quotes['FinalKey'].str.upper()
 
- if open_orders.empty:
+    # Open Orders
+    # Check if open_orders is empty
+    if open_orders.empty:
         st.warning('Open Orders file is empty.')
     else:
         # Continue with processing
@@ -50,7 +51,8 @@ if ppv_offers_file and sat_quotes_file and open_orders_file:
 
     # Merging
     merged_data = ppv_offers.merge(sat_quotes, on='FinalKey', how='left')
-    merged_data = merged_data.merge(open_orders, on='FinalKey', how='left', validate='many_to_many' if not open_orders.empty else 'one_to_many')
+    if not open_orders.empty:
+        merged_data = merged_data.merge(open_orders, on='FinalKey', how='left')
 
     # Drop specified columns
     merged_data = merged_data.drop(columns=['FinalKey', 'Offer Site', 'STD Site', 'Offer JPN', 'STD JPN', 'STD MPN'])
