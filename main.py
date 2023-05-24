37f5994e1444470e6ee9a4bd7360f92d3bf06950
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2023-05-24 15:31:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import pandas as pd
 import base64
+import datetime
 
 def get_table_download_link(df):
     """Generates a link allowing the data in a given panda dataframe to be downloaded
     in:  dataframe
     out: href string
     """
+    # Get current day of the week
+    day_of_week = datetime.datetime.now().strftime('%A')
+    
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}" download="merged_data.csv">Download csv file</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="PPV_{day_of_week}.csv">Download csv file</a>'
     return href
 
 # Display a file uploader widget in your app for each Excel file
